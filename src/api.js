da3e24766fc113ea71ad3be80a607e5afe2e63da
Eddie-Sc <scales.ed10@hotmail.co.nz>
2022-08-18 11:02:42 +1200

    test
    test

--- src/api.js
+++ src/api.js
@@ -2,12 +2,19 @@ const express = require("express")
 const serverless = require ("serverless-http")
 
 
+
 const app = express()
 const router = express.Router()
 
 router.get("/", (req, res) => {
     res.send("User List")
 })
+
+
+
+
+
+
 
 router.post("/", (req, res) => {
     res.send("Create New User")
